14c42b99aeab0d5ace8a51e88310f0af95dc483b
Jin Pyo Jeon <jinpyo.jeon@trincoll.edu>
2019-05-28 11:40:52 -0400

    Add files via upload

--- convert-books.sh
+++ convert-books.sh
@@ -1,7 +1,16 @@
 IFS=$'\n'
-for file in `ls -1 | grep epub`
-do
-    baseFilename=`basename $file .epub`
-    echo "${baseFilename}"
-    ebook-convert "${baseFilename}.epub" "${baseFilename}.mobi"
-done
+
+if [ -z "$1" ]; then
+    for file in `ls -1 | grep epub`
+    do
+        baseFilename=`basename $file .epub`
+        echo "${baseFilename}"
+        ebook-convert "${baseFilename}.epub" "${baseFilename}.mobi"
+    done
+else
+    echo `$1`
+    baseFileName=`basename $1 .epub`
+    echo "${baseFileName}"
+    ebook-convert "${baseFileName}.epub" "${baseFileName}.mobi"
+fi
+
